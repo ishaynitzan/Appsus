f0c16ded6df15244590bb7142c9ae20dc2509948
ishaynitzan <ishaynitzan@gmail.com>
2021-11-14 09:27:28 +0200

    header add

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -11,10 +11,10 @@ export default {
         </div>
         <nav class="flex space-evenly align-center">
         <h1>Appsus</h1>
-        <router-link class="link-btn" to="/">home</router-link> 
+        <router-link class="link-btn" to="/">Home</router-link> 
         <router-link class="link-btn" to="/email">Email</router-link> 
         <router-link class="link-btn" to="/notes">Notes</router-link> 
-        <router-link class="link-btn" to="/about">about</router-link> 
+        <router-link class="link-btn" to="/about">About</router-link> 
         <div>
           ⌬
         </div>
